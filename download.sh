f2bd2dc249a673081c4ae8965f9cd1c0386ca5da
Jack Markrest <if1033103081@gmail.com>
2020-02-04 07:34:26 +0800

    add fcitx pkg download script

---
+++ download.sh
@@ -0,0 +1,1 @@
+sudo apt-get install fcitx fcitx-table fcitx-ui-classic fcitx-frontend-all fcitx-config-common fcitx-config-gtk fcitx-config-gtk2 fcitx-tools fcitx-module-dbus fcitx-libs-qt libopencc2
