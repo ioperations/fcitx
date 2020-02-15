8b5b664226b6c826ba8830813626dc59015fe004
Jack Markrest <if1033103081@gmail.com>
2020-02-15 21:30:28 +0800

    stable

--- download.sh
+++ download.sh
@@ -1,1 +1,2 @@
-sudo apt-get install fcitx fcitx-table fcitx-ui-classic fcitx-frontend-all fcitx-config-common fcitx-config-gtk fcitx-config-gtk2 fcitx-tools fcitx-module-dbus fcitx-libs-qt libopencc2
+#sudo pacman -S fcitx fcitx-table fcitx-ui-classic fcitx-frontend-all fcitx-config-common fcitx-config-gtk fcitx-config-gtk2 fcitx-tools fcitx-module-dbus fcitx-libs-qt libopencc2
+sudo pacman -S fcitx 
