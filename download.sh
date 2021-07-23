59433434160c20781907c39e9af16f8dfc72cf43
aodong.qin <ioperations.c@gmail.com>
2021-07-23 10:08:15 +0800

    update

--- download.sh
+++ download.sh
@@ -1,2 +1,1 @@
-#sudo pacman -S fcitx fcitx-table fcitx-ui-classic fcitx-frontend-all fcitx-config-common fcitx-config-gtk fcitx-config-gtk2 fcitx-tools fcitx-module-dbus fcitx-libs-qt libopencc2
-sudo pacman -S fcitx 
+sudo apt install -y fcitx fcitx-rime
